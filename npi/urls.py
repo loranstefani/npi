405fb696ea30c8af1e6fffffb8939f3ece822eb6
Alan Viars <alan.c.viars@gmail.com>
2013-12-20 16:00:02 -0500

    Worked in bootstrap theme

--- npi/urls.py
+++ npi/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,6 +14,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^$',   TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^enumerations/', include('apps.enumerations.urls')),
 )
