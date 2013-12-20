d410ff8d6321d89f18e326138a53442602361acb
Alan Viars <alan.c.viars@gmail.com>
2013-12-20 09:43:38 -0500

    Add bootstrap form and localflavor

--- npi/settings.py
+++ npi/settings.py
@@ -130,10 +130,16 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
+    #'django.contrib.admindocs',
+    
+    #3rd party
+    'bootstrapform',
     'localflavor',
+    
+    #our apps
     'apps.taxonomy',
     'apps.enumerations',
+    
 )
 
 # A sample logging configuration. The only tangible logging
