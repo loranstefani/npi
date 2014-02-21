b3c65037245251b26c87e17b0f6cf16b22f6ed8d
Alan Viars <alan.c.viars@gmail.com>
2014-02-21 14:15:55 -0500

    Added AJAX Select to Surrogate Admin

--- apps/surrogates/admin.py
+++ apps/surrogates/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from models import Surrogate, SurrogateRequest
+from ajax_select import make_ajax_form
+from ajax_select.admin import AjaxSelectAdmin
 
 
-
-class SurrogateAdmin(admin.ModelAdmin):
+class SurrogateAdmin(AjaxSelectAdmin):
     search_fields = ('user', )
+    form = make_ajax_form(Surrogate, {'user': 'manager',
+                                       'enumerations': 'enumeration', 
+                                    })
+
+
 admin.site.register(Surrogate, SurrogateAdmin)
 
-admin.site.register(SurrogateRequest)
+
+
+class SurrogateRequestAdmin(AjaxSelectAdmin):
+    search_fields = ('user', )
+    list_display = ('user', 'enumeration', 'added')
+    form = make_ajax_form(SurrogateRequest, {'user': 'manager',
+                                       'enumeration': 'enumeration', 
+                                    })
+
+
+admin.site.register(SurrogateRequest, SurrogateRequestAdmin)
