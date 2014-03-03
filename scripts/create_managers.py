dd5fd59f79ffad3b58f2af095684f1c7b2e69e42
Alan Viars <alan.c.viars@gmail.com>
2014-03-03 14:37:52 -0500

    Add a few managers to each enumeration for each user.  For testing

---
+++ scripts/create_managers.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4
+
+import sys, csv
+from django.conf import settings
+import datetime
+from apps.enumerations.models import Enumeration
+from apps.surrogates.models import Surrogate
+from django.contrib.auth.models import User
+
+
+
+def create_managers():
+    
+    #users = User.objects.all()
+    
+    u = User.objects.get(username="alan")
+    s = Surrogate.objects.get(user=u)
+    enums = Enumeration.objects.all()[:10]
+    for e in enums:
+        s.enumerations.add(e)
+        e.managers.add(u)
+   
+    print "Done."
+            
+def run():
+
+    try:
+        create_managers()
+    except:
+        print "Error."
+        print sys.exc_info()
