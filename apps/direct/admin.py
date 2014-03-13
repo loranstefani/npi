0a2e69ba33a3841cc1fa6d1e284ef3659b707a01
Alan Viars <alan.c.viars@gmail.com>
2014-03-13 16:42:18 -0400

    Allow Direct email addresses and Direct certificates to be created seperately

--- apps/direct/admin.py
+++ apps/direct/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from models import DirectAddress
+from models import DirectAddress, DirectCertificate
 
 
 
 class DirectAddressAdmin(admin.ModelAdmin):
 
-    list_display = ('email', 'dns', 'status', 'verified',)      
+    list_display = ('email', 'dns', 'status', 'verified', 'updated')      
     search_fields = ('email', 'dns',)
 
 admin.site.register(DirectAddress, DirectAddressAdmin)
 
+class DirectCertificateAdmin(admin.ModelAdmin):
+
+    list_display = ('__unicode__', 'dns', 'updated')      
+    search_fields = ('dns', 'updated',)
+
+admin.site.register(DirectCertificate, DirectCertificateAdmin)
--- apps/direct/models.py
+++ apps/direct/models.py
@@ -7,15 +7,11 @@ class DirectAddress(models.Model):
     organization  = models.CharField(max_length=150,  default="")
     dns           = models.CharField(max_length=150,  blank=True, default="",
                                     editable=False, db_index=True)
-    certificate    = models.FileField(blank = True, null=False, default='',
-                                    max_length=255L, upload_to="direct_pems",
-                                    verbose_name= "Certificate File")
+    status      = models.CharField(max_length=10, blank=True,default ="")
+    verified    = models.BooleanField(default=False, editable=False)
+    added       = models.DateField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
 
-    status         = models.CharField(max_length=10, blank=True,default ="")
-    verified       = models.BooleanField(default=False, editable=False)
-   
-
-    
     
     def __unicode__(self):
         return self.dns
@@ -25,3 +21,26 @@ class DirectAddress(models.Model):
         
         self.dns = self.email.replace("@", ".")     
         super(DirectAddress, self).save(**kwargs)
+        
+        
+        
+class DirectCertificate(models.Model):
+    dns         = models.CharField(max_length=3000,  blank=True, default="",
+                                    #editable=False,
+                                    db_index=True)
+    file        = models.FileField(blank = True, null=False, default='',
+                        max_length=255L, upload_to="direct_pems",
+                        verbose_name= "Direct Public Certificate",
+                        help_text ="This is an x509 Public Certificate in PEM format.")
+    added       = models.DateField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+   
+
+    
+    def __unicode__(self):
+        return str(self.id)
+
+
+    def save(self, **kwargs):
+         
+        super(DirectCertificate, self).save(**kwargs)
