31c15bba639f9f5f5c216288af0552a31ae3cecf
Alan Viars <alan.c.viars@gmail.com>
2014-07-31 17:36:29 -0400

    Widen specialty description column to match data.

--- apps/specialties/models.py
+++ apps/specialties/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class SpecialtyCode(models.Model):
     code           = models.CharField(max_length=3, unique=True, db_index=True)
-    description    = models.CharField(max_length=100, unique=True)
+    description    = models.TextField(max_length=300, unique=True)
     taxonomy       = models.CharField(max_length=20, blank=True, default="", db_index=True)
     
     def __unicode__(self):
